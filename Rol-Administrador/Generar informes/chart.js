7cefa423eb303b749f8181d0d856990467f9836f
Erick1506 <163191519+Erick1506@users.noreply.github.com>
2025-02-17 15:20:14 -0500

    Delete Rol-Administrador directory

--- Rol-Administrador/Generar informes/chart.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    const startDate = document.getElementById('start-date').value;
-    const endDate = document.getElementById('end-date').value;
-
-    if (new Date(startDate) > new Date(endDate)) {
-        e.preventDefault();
-        alert('La fecha de inicio no puede ser mayor que la fecha de fin.');
-    }
-});
